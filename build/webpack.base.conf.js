bb3e90e44d6aefd28a16cd88b2a9e2e7d0b68a12
weaps <weaps@qq.com>
2018-09-20 21:35:23 +0800

    'update'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'pages': resolve('src/pages'),
+      'css': resolve('src/assets/css'),
     }
   },
   module: {
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">Home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="stylus" scoped>
+  body
+    padding 0
+    margin 0
+</style>
